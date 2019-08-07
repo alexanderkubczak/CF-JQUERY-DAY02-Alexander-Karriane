cca156b562b0bb568583f7139d49e2e6d28084da
Karianne Dramdal <karianne.dramdal@live.no>
2019-08-07 11:00:47 +0200

    renders items with jquery

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -1,33 +1,15 @@
+
+
 $(document).ready(function(){
 
-for (var i=0; i < items.length; i++);
-	$("items").prepend(`
+for (var i=0; i < items.length; i++) {
+	$("#container").prepend(`
+		<div class="itembox">
 		<img src="${items[i].image}">
 		<p>Price: ${items[i].price}€</p>
+		<p>${items[i].name}</p>
 		<p>Calories: ${items[i].calories}</p>
-
-		`)
-
-
+		</div>`);}
+});
 
 
-
-
-	for (var i=0; i<groceries.length; i++){
-		$("#grocerybox").prepend(`
-			<div class="item">
-				<img src="${groceries[i].image}">
-				<p>${groceries[i].product}</p>
-				<p>Price: ${groceries[i].price}€</p>
-				<button class="status${i}"></button>
-			</div>
-			`);
-		if (groceries[i].quantity > 0) {
-			$(`.status${i}`).text("AVAILABLE");
-		}
-		else {
-			$(`.status${i}`).text("OUT OF STOCK");
-		}
-
-	}
-})
